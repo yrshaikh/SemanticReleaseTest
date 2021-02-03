9c6cae8e28d6893166e890571010c161d72c0ee2
Yasser shaikh <yrshaikh.mail@gmail.com>
2021-02-03 11:07:33 +0700

    fix(semanticrelease): updated release config to add echo for prepare and publish

--- release.config.js
+++ release.config.js
@@ -1,3 +1,18 @@
 module.exports = {
-    branches: ['main']
+    branches: ['main'],
+    "verifyConditions": ["@semantic-release/github"],
+    "prepare": 
+    [
+        {
+            "path": "@semantic-release/exec",
+            "cmd": "echo from prepare ${nextRelease.version}"
+        }
+    ],
+    "publish": [
+        "@semantic-release/github",
+        {
+            "path": "@semantic-release/exec",
+            "cmd": "echo from publish ${nextRelease.version}"
+        }
+    ],
 }
