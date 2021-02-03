b06259fe43368ae1941f1fadeaad779ccf9b6637
Yasser Shaikh <yasser.shaikh@agoda.com>
2021-02-03 09:50:37 +0700

    feat(helloworld): added lib to return "helloworld"

---
+++ Library/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Library
+{
+    public class Class1
+    {
+        public static string GetHelloWorld()
+        {
+            return "HelloWorld";
+        }
+    }
+}
