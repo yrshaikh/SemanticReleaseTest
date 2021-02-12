332d0ca735cdb243e3ab7f10e04fe16a3fd1e3cc
Yasser Shaikh <yasser.shaikh@agoda.com>
2021-02-12 20:38:45 +0700

    fix: revert

--- release.config.js
+++ release.config.js
@@ -1,10 +1,5 @@
 module.exports = {
     branches: ['main'],
-    "plugins": [
-        "@semantic-release/git",
-        "@semantic-release/exec",
-        "@semantic-release/changelog"
-    ],
     "verifyConditions": ["@semantic-release/github"],
     "prepare": 
     [
